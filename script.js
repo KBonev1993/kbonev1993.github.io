57e1aaca873f3082f3297acee184d94012bd3512
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2024-12-29 15:18:21 +0200

    Update script.js

--- script.js
+++ script.js
@@ -15,10 +15,3 @@ document.addEventListener('mouseleave', () => {
   cursorRing.style.width = '0';
   cursorRing.style.height = '0';
 });
-// Custom Mouse Cursor
-const cursor = document.querySelector('.custom-cursor');
-
-document.addEventListener('mousemove', (e) => {
-    cursor.style.left = `${e.pageX}px`;
-    cursor.style.top = `${e.pageY}px`;
-});
