6786421c4adb89075da3476747285690893e1ecb
Kristiyan Bonev <85802301+KBonev1993@users.noreply.github.com>
2024-12-29 15:11:48 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+// Cursor ring effect
+const cursorRing = document.createElement('div');
+cursorRing.classList.add('cursor-ring');
+document.body.appendChild(cursorRing);
+
+document.addEventListener('mousemove', (e) => {
+  const { clientX, clientY } = e;
+  cursorRing.style.left = `${clientX}px`;
+  cursorRing.style.top = `${clientY}px`;
+  cursorRing.style.width = '30px';
+  cursorRing.style.height = '30px';
+});
+
+document.addEventListener('mouseleave', () => {
+  cursorRing.style.width = '0';
+  cursorRing.style.height = '0';
+});
 // Custom Mouse Cursor
 const cursor = document.querySelector('.custom-cursor');
 
